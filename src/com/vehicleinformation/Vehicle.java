820f8924afd109f5e74df5abc28c84f51f9c5efb
wandilemawelela <wandile1997@gmail.com>
2025-12-26 05:13:38 +0200

    Initial commit

---
+++ src/com/vehicleinformation/Vehicle.java
@@ -0,0 +1,11 @@
+package com.vehicleinformation;
+
+/**
+ * Vehicle interface defines common behavior for all vehicle types.
+ */
+
+public interface Vehicle {
+	String getMake();
+    String getModel();
+    int getYear();
+}
